4d0047362e8f236a05ebe3ef46382caf0fad1a09
Connor Bond <connorbond@driveworks.co.uk>
2022-03-18 15:23:59 +0000

    1.0.0 - Initial release

---
+++ config.js
@@ -0,0 +1,9 @@
+// Update these values to match those of your Server URL, DriveWorks Group Alias
+// and Project Name that you wish to be rendered. Optionally, set a Constant Name to drive.
+
+const config = {
+    serverUrl: "",
+    groupAlias: "",
+    projectName: "RequestScreenshot",
+    constantName: "ModelColor", // optional
+};
